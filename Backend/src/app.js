d8444035c366c7ba7a3dd0337dff62ac9d397980
Sk Md Afsaruddin <skmdafsaruddin@Sks-MacBook-Air.local>
2026-05-01 08:08:16 +0530

    update CORS for production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5175",
+    origin: process.env.FRONTEND_URL || "http://localhost:5175", // 👈 fixed
     credentials: true,
   }),
 );
